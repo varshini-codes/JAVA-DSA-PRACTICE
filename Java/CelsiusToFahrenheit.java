ddcf6b7fa8ced1eaff5849db1ae36b25f41eb45c
varshini-codes <varshiniprasadr05@gmail.com>
2026-07-04 18:31:24 +0530

    Create CelsiusToFahrenheit.java

---
+++ Java/CelsiusToFahrenheit.java
@@ -0,0 +1,9 @@
+public class CelsiusToFahrenheit {
+    public static void main(String[] args) {
+        double celsius = 30;
+
+        double fahrenheit = (celsius * 9 / 5) + 32;
+
+        System.out.println("Temperature in Fahrenheit = " + fahrenheit);
+    }
+}
