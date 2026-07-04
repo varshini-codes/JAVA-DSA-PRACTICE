1e5dc743e784e7abeed7f6b17b68705b5ef2d4c2
varshini-codes <varshiniprasadr05@gmail.com>
2026-07-04 18:40:35 +0530

    Create CountVowels.java

---
+++ Java/CountVowels.java
@@ -0,0 +1,15 @@
+public class CountVowels {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = Character.toLowerCase(str.charAt(i));
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+                count++;
+        }
+
+        System.out.println("Vowels = " + count);
+    }
+}
