6617c3ea273904b8e5c179c90d5bdfaead0d5777
varshini-codes <varshiniprasadr05@gmail.com>
2026-07-04 18:37:18 +0530

    Create SumOfNaturalNumbers.java

---
+++ Java/SumOfNaturalNumbers.java
@@ -0,0 +1,11 @@
+public class SumOfNaturalNumbers {
+    public static void main(String[] args) {
+        int n = 10, sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
