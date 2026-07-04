72711aca68ea316e8ff8aeb941d137cdaf946c7c
varshini-codes <varshiniprasadr05@gmail.com>
2026-07-04 18:32:17 +0530

    Create AverageOfNumbers.java

---
+++ Java/AverageOfNumbers.java
@@ -0,0 +1,7 @@
+public class AverageOfNumbers {
+    public static void main(String[] args) {
+        int a = 10, b = 20, c = 30;
+        double avg = (a + b + c) / 3.0;
+        System.out.println("Average = " + avg);
+    }
+}
