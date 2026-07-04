89d22fd222d40bafbf594a9e43ca7991266145ed
varshini-codes <varshiniprasadr05@gmail.com>
2026-07-04 18:41:53 +0530

    Create RemoveSpaces.java

---
+++ Java/RemoveSpaces.java
@@ -0,0 +1,6 @@
+public class RemoveSpaces {
+    public static void main(String[] args) {
+        String str = "Java Programming Language";
+        System.out.println(str.replace(" ", ""));
+    }
+}
