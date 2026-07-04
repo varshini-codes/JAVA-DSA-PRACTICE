f22864c611aff0c435bf3192acea9ddb4d770181
varshini-codes <varshiniprasadr05@gmail.com>
2026-07-04 18:40:10 +0530

    Create PalindromeString.java

---
+++ Java/PalindromeString.java
@@ -0,0 +1,15 @@
+public class PalindromeString {
+    public static void main(String[] args) {
+        String str = "madam";
+        String rev = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += str.charAt(i);
+        }
+
+        if (str.equals(rev))
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+    }
+}
