1be66ee61ac89a34326f57f0bcfe4203749cc76b
varshini-codes <varshiniprasadr05@gmail.com>
2026-07-04 18:32:57 +0530

    Create PositiveNegativeZero.java

---
+++ Java/PositiveNegativeZero.java
@@ -0,0 +1,12 @@
+public class PositiveNegativeZero {
+    public static void main(String[] args) {
+        int num = -5;
+
+        if (num > 0)
+            System.out.println("Positive");
+        else if (num < 0)
+            System.out.println("Negative");
+        else
+            System.out.println("Zero");
+    }
+}
