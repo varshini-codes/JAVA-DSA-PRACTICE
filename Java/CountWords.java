beb7c2b1c5ba260071a37bcd5d5390cbfccf3ea6
varshini-codes <varshiniprasadr05@gmail.com>
2026-07-04 18:41:03 +0530

    Create CountWords.java

---
+++ Java/CountWords.java
@@ -0,0 +1,7 @@
+public class CountWords {
+    public static void main(String[] args) {
+        String str = "Java is easy";
+        String[] words = str.split(" ");
+        System.out.println("Words = " + words.length);
+    }
+}
